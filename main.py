8bddb596a485672a4bbfa05287bed49978b955dd
guygnakpa <guygnakpa@gmail.com>
2022-06-02 22:48:11 -0400

    Portfolio displaying analytical research, Defi analysis, System Architecture Analysis and Financial analysis; case studies.

--- main.py
+++ main.py
@@ -708,7 +708,7 @@ if Nav_Menu == "Stablecoins Analysis":
     #insert CBDCs Architecture
     CBDC_col1,CBDC_col2,=st.columns(2)
     with CBDC_col1:
-        CBDC_image0 = Image.open("CBDC  Architecture3.png")
+        CBDC_image0 = Image.open("CBDC Architecture3.png")
         st.image(CBDC_image0, width=645)
     with CBDC_col2:
         st.info("""
