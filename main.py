f60c7a15c01fd0bb80f68ba68a344ef055132f90
guygnakpa <guygnakpa@gmail.com>
2023-01-29 16:40:24 -0500

    Merge remote-tracking branch 'origin/master'

--- main.py
+++ main.py
@@ -799,7 +799,7 @@ if Nav_Menu == "Capital Markets Analysis":
         etf_info = yf.Ticker(etf)
         # Extract the market cap from the ETF's information
         market_cap = etf_info.fast_info["market_cap"]
-        st.write(market_cap)
+        #st.write(market_cap)
         # Add a new row to the DataFrame with the ETF's ticker and market cap
         market_cap_df = market_cap_df.append({"ETF": etf, "Market Cap": market_cap}, ignore_index=True)
     st.dataframe(market_cap_df)
